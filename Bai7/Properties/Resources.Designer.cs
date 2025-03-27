d1f70c19db4e6b2a4eae5553718c0f34a4b1604b
thuanquang <ghoulbulldog@gmail.com>
2025-03-27 23:58:45 +0700

    ád

--- Bai7/Properties/Resources.Designer.cs
+++ Bai7/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Bai7.Properties {
+namespace Module1Bai7_TranThuanQuang.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Bai7.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Bai7.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Module1Bai7_TranThuanQuang.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
